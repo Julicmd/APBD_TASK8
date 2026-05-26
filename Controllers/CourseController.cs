f559749b0226a5ea17d5f1238e717c91c1684930
Julius Babelis <s31850@pjwst.edu.pl>
2026-05-26 14:31:09 +0200

    Course Controller get Endpoint

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -1,6 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversityTasksDbFirstApi.Data;
+using UniversityTasksDbFirstApi.DTOs;
+using UniversityTasksDbFirstApi.Models;
+
 namespace UniversityTasksDbFirstApi.Controllers;
 
-public class CourseController: BaseC
+[ApiController]
+[Route("api/courses")]
+public class CourseController: ControllerBase
 {
+    private readonly UniversityTasksDbContext _dbContext;
+    
+    public  CourseController(UniversityTasksDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    //Method Endpoint Required logic GET /api/courses?activeOnly=true
+    //Return courses with assignment counts. Use AsNoTracking()and projection to DTO
+    [HttpGet]
+    public async Task<IActionResult> GetAllActiveCourses([FromQuery] bool activeOnly = false)
+    {
+        var course = await _dbContext.Courses.AsNoTracking()
+            .Include(c=>c.Assignments)
+            .Where(c => !activeOnly || c.IsActive)
+            .ToListAsync();
+
+
+        var result = course.Select(coa => new CourseDto
+        {
+            courseId = coa.CourseId,
+            code = coa.Code,
+            name = coa.Name,
+            credits = coa.Credits,
+            countAssignments =  coa.Assignments.Count
+        });
+        
+        return Ok(result);
+    }
+    
+    //GET /api/courses/{idCourse}/assignments?publishedOnly=true Return assignments for one course.
+    //Include submission count. Return 404 if the course does not exist.
+    [HttpGet("{idCourse:int}/assignments")]
+    public async Task<IActionResult> GetAssignments([FromRoute] int idCourse)
+    {
+        
+    }
     
 }
